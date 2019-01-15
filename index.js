a5f8d62625ec8757023db85b021c5c977ab9e9e1
SoChoukah <43469554+SoChoukah@users.noreply.github.com>
2019-01-15 16:27:49 -0500

    Finished integrating webpage
    Finished styling landing + a bit of JS + added a missing image and fixed an svg file.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+
+// Step 1: console.log the input field
+// -------------  **** THIS WORKS!! ...But doesn't update when the field is updated..--------------  ****
+var elInputOne = document.getElementsByClassName("form-field")[0].value;
+document.oninput = consoleData;
+
+var elInputTwo = document.getElementsByClassName("form-field")[1].value;
+document.oninput = consoleData;
+
+var elButton = document.getElementById("special-button");
+// Step 2: When click on Search button, console.log the inputs.
+
+function consoleData() {
+    if (elInputOne.length >= 3 && elInputTwo.length >= 3) {
+        console.log(elInputOne + " " + elInputTwo);
+    }
+}
+
+//Step 3: Update the text on the button when clicked 
+// -------------  **** THIS WORKS!! --------------  ****
+
+// var elButton = document.getElementById("special-button");
+elButton.addEventListener('click', function buttonUpdate() {
+    elButton.innerHTML = "Thanks!";
+});
